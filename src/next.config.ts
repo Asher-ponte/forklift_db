1ff14d7983b6f116b75abd453b9360b48d8db9d6
Florante Catapang <florantecatapang6@gmail.com>
2025-06-11 02:32:24 +0000

    studio-3148978742:~/studio{master}$ npm run dev -- -H 0.0.0.0
    > nextn@0

---
+++ src/next.config.ts
@@ -0,0 +1,27 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: ['https://9003-firebase-studio-1749437046486.cluster-zkm2jrwbnbd4awuedc2alqxrpk.cloudworkstations.dev'],
+  },
+};
+
+export default nextConfig;
